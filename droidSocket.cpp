2670cf128791a4ab26cf4baf4b60e38eeabcf8fe
a6ar55 <adars.nambootiri@gmail.com>
2025-09-20 19:17:46 +0530

    Socket object created git add .

---
+++ droidSocket.cpp
@@ -0,0 +1,27 @@
+#include<./droidSocket.h>
+
+    void droid::droidSocket::test_connection(int connect){
+        if(connect<0){
+            perror("Failed to connect !! 🥹");
+            exit(EXIT_FAILURE);
+        }
+    }
+    droid::droidSocket::droidSocket(int domain,int type,int protocol,int port,u_long machineIp){
+        adress.sin_family=domain;
+        adress.sin_port=port;
+        //htonl converts host byte order to network byte order(big endian)
+        adress.sin_addr.s_addr=htonl(machineIp);
+
+        sock=socket(domain,type,protocol);
+        test_connection(sock);
+        connection=connect_to_network(sock,(struct sockaddr *)&adress,sizeof(adress));
+        test_connection(connection);
+
+    }
+    int droid::droidSocket::get_connection(){
+        return connection;
+    }
+
+    int droid::droidSocket::get_sock(){
+        return sock;
+    }
--- droidSocket.h
+++ droidSocket.h
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<sys/socket.h>
 #include<stdio.h>
+#include<netinet/in.h>
+#define PORT=8080
 
 //type means to specify the type of protocol
 //like is it SOCK_STREAM (gen used for http server)or SOCK_DGRAM etc
@@ -9,10 +11,16 @@
 namespace droid{
 class droidSocket{
     private:
+    struct sockaddr_in adress;
+    int sock;
     int connection;
     public:
-    droidSocket(int protocol,int domain,int type);
-    bool test_connection(int connection);
+    droidSocket(int domain,int type,int protocol,int port,u_long machineIp);
+    void test_connection(int connect);
     int get_connection();
+    int get_sock();
+    //to check whether to use bind() or connect() based on server or client
+    //vurtual helps to implement run time polymorphism so child function overrides parent
+    virtual int connect_to_network(int sock,struct sockaddr * adress,int len)=0;
 };
 }
